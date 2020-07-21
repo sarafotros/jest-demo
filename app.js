1e79c06f1ac3de4e90b62c40f9415b8402dd0e3d
sarafotros <sarafotros@gmail.com>
2020-07-21 16:40:10 +0100

    toMatchobject

--- app.js
+++ app.js
@@ -7,7 +7,24 @@ const sumNum = (input) => {
 
 const eqSt = (st1, st2) => (st1 === st2 ? true : false);
 
+const initial = (inp) => {
+  return inp
+    .split(' ')
+    .map((n) => n[0])
+    .join(' ');
+};
+
+const evenOdd = (mixed) => {
+  let even = [];
+  let odd = [];
+  for (let mix of mixed) {
+    mix % 2 === 0 ? even.push(mix) : odd.push(mix);
+  }
+  return { odd, even };
+};
 module.exports = {
   sumNum,
   eqSt,
+  initial,
+  evenOdd,
 };
--- app.test.js
+++ app.test.js
@@ -1,5 +1,5 @@
 const { add } = require('./app2');
-const { sumNum, eqSt } = require('./app');
+const { sumNum, eqSt, initial, evenOdd } = require('./app');
 
 test('Should equal 7 when 3 and 4 are passed', () => {
   expect(add(3, 4)).toEqual(7);
@@ -11,4 +11,18 @@ test('should add all the numbers in the array and return the sum ', () => {
 
 test('should return true when "code" and "code" are passed', () => {
   expect(eqSt('code', 'code')).toBeTruthy();
+});
+
+test('should return "B" and "M" when "Ben Maudslay" is passed ', () => {
+  expect(initial('Ben Maudslay')).toMatch(/B M/);
+});
+
+test('should return even:[2,4] when receive [1,2,3,4] ', () => {
+  //   const Objet = {
+  //     even: [2, 4],
+  //     odd: [1, 3],
+  //   };
+  //   expect(evenOdd([1, 2, 3, 4])).objectContaining('even:[2,4]');
+  //   expect(Object.even).toBe([2, 4]);
+  expect(evenOdd([1, 2, 3, 4])).toMatchObject({ even: [2, 4] });
 });
